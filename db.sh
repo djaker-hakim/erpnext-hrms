b4daed8209c41db1f0c36e8ee2b7be884273fbe2
djaker hakim <hakiltheknight@gmail.com>
2026-03-13 17:28:59 +0100

    docker compose+ env + db

---
+++ db.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+# this is a docker command to simulate a stand alone db server
+
+docker run -d \
+  --name db \
+  -e MYSQL_ROOT_PASSWORD=admin \
+  -e MARIADB_ROOT_PASSWORD=admin \
+  -p 3306:3306 \
+  -v db-data:/var/lib/mysql \
+  mariadb:10.6 \
+  --character-set-server=utf8mb4 \
+  --collation-server=utf8mb4_unicode_ci \
+  --skip-character-set-client-handshake \
+  --skip-innodb-read-only-compressed
